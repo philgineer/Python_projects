425d3d1feea53f980a86017265341330d5764fcc
Philgineer <43261253+yunjh0420@users.noreply.github.com>
2020-06-23 14:54:12 +0900

    completed

--- blockchain/backend/app/__init__.py
+++ blockchain/backend/app/__init__.py
@@ -3,6 +3,7 @@ import random
 import requests
 
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from backend.blockchain.blockchain import Blockchain
 from backend.pubsub import PubSub
@@ -11,6 +12,7 @@ from backend.wallet.transaction_pool import TransactionPool
 from backend.wallet.transaction import Transaction
 
 app = Flask(__name__)
+CORS(app, resources={ r'/*': {'origins': 'http://localhost:3000'}})
 blockchain = Blockchain()
 wallet = Wallet(blockchain)
 transaction_pool = TransactionPool()
@@ -23,6 +25,18 @@ def route_default():
 @app.route('/blockchain')
 def route_blockchain():
     return jsonify(blockchain.to_json())
+
+@app.route('/blockchain/range')
+def route_blockchain_range():
+    # http://localhost:5000/blockchain/range?start=2&end=5
+    start = int(request.args.get('start'))
+    end = int(request.args.get('end'))
+    
+    return jsonify(blockchain.to_json()[::-1][start:end])
+
+@app.route('/blockchain/length')
+def route_blockchain_length():
+    return jsonify(len(blockchain.chain))
 
 @app.route('/blockchain/mine')
 def route_blockchain_mine():
@@ -61,6 +75,20 @@ def route_wallet_transact():
 def route_wallet_info():
     return jsonify({'address': wallet.address, 'balance': wallet.balance})
 
+@app.route('/known-addresses')
+def route_known_addresses():
+    known_addresses = set()
+    
+    for block in blockchain.chain:
+        for transaction in block.data:
+            known_addresses.update(transaction['output'].keys())
+            
+    return jsonify(list(known_addresses))
+
+@app.route('/transactions')
+def route_transactions():
+    return jsonify(transaction_pool.transaction_data())
+
 ROOT_PORT = 5000
 PORT = ROOT_PORT
 
@@ -75,5 +103,17 @@ if os.environ.get('PEER') == 'True':
         print('\n -- Successfully synchronized the local chain')
     except Exception as e:
         print(f'\n -- Error synchronizing: {e}')
+        
+if os.environ.get('SEED_DATA') == 'True':
+    for i in range(10):
+        blockchain.add_block([
+            Transaction(Wallet(), Wallet().address, random.randint(2,50)).to_json(),
+            Transaction(Wallet(), Wallet().address, random.randint(2,110)).to_json(),
+        ])
+        
+    for i in range(3):
+        transaction_pool.set_transaction(
+            Transaction(Wallet(), Wallet().address, random.randint(9,70))
+        )
 
 app.run(port=PORT)
